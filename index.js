df8044482f02c01f7c064b325a8fe5f74547e2a2
KannangAndroid <135958944+KannangAndroid@users.noreply.github.com>
2023-07-25 14:44:07 +0530

    Update index.js
    updated port

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ export const config = {
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'nibav-staging',
   masterKey: process.env.MASTER_KEY || 'lncPLVCkjb3sk24324knvdncvKJN', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'http://localhost:5432/parse', // Don't forget to change to https if needed
   liveQuery: {
     classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
   },
@@ -47,7 +47,7 @@ app.get('/test', function (req, res) {
 });
 
 if (!process.env.TESTING) {
-  const port = process.env.PORT || 1337;
+  const port = process.env.PORT || 5432;
   const httpServer = http.createServer(app);
   httpServer.listen(port, function () {
     console.log('parse-server-example running on port ' + port + '.');
